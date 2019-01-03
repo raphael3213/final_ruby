32de3c262d0fa049e3fe4f6a8f2206d3b29eb3fa
Joel Peter <raphael3213@gmail.com>
2019-01-03 14:09:14 +0530

    done

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,46 @@
+class UsersController < ApplicationController
+ 
+	def show
+	@user=User.find(params[:id])
+	
+	end 
+ 
+ 
+  def new
+  @user=User.new
+  end
+  
+  def create
+  @user=User.new(user_params)
+  if @user.save
+  name=@user.name
+  flash[:success]="Welcome #{name}";
+  log_in @user
+  redirect_to @user
+  else
+  render 'new'
+  end
+  end
+  
+
+  def edit
+  	@user=User.find(params[:id])
+
+  end
+
+  def update
+    @user=User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #done
+    else
+      #notdone
+    end
+  end
+
+  private
+  
+  def user_params
+  params.require(:user).permit(:name,:email,:password,:password_confirmation)
+  end
+  
+end
